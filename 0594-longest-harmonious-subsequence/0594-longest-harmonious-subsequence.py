23767c753eec281c8ac790245e1dd4c18f26fd9a
vaibhav sharma <hs404021.vs45@gmail.com>
2026-08-12 01:47:26 +0530

    Time: 63 ms (30.38%), Space: 13.3 MB (86.92%) - LeetHub

---
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findLHS(self, nums):
+        nums.sort()
+        left = 0
+        ans = 0
+        for right in range(len(nums)):
+            while nums[right] - nums[left] > 1:
+                left +=1
+            if nums[right] - nums[left] ==1:
+                ans = max(ans,right-left+1)
+        return ans
