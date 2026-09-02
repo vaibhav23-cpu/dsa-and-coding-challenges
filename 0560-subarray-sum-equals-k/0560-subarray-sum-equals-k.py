e468e6e451c6e20c392cdc53e022fba8564ddd58
vaibhav sharma <hs404021.vs45@gmail.com>
2026-09-02 16:32:24 +0530

    Time: 52 ms (6.48%), Space: 15.3 MB (18.48%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,13 +1,13 @@
 class Solution(object):
     def subarraySum(self, nums, k):
         #sbse pehle ek empty dictionary bnaege fir compare krege array ko current number se agar vo sum 'k' k equal hota h toh usko seen m add krdege or tbtk krege jbtk k or sum dono equal nhi hojate
-        seen = {0: 1}
-        prefix = 0
-        count = 0
-        for num in nums:
-            prefix += num
-            need = prefix - k
+        seen = {0:1}
+        sum_num = 0
+        res = 0
+        for i in range(len(nums)):
+            sum_num+= nums[i]
+            need = sum_num - k
             if need in seen:
-                count += seen[need]
-            seen[prefix] = seen.get(prefix, 0)+1
-        return count
+                res += seen[need]
+            seen[sum_num] = seen.get(sum_num,0)+1
+        return res
